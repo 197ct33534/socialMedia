a60f96794e08e6c0d8be6cfe3ee4eb4d55f0979c
unknown <trantrungnghia07122001@gmail.com>
2022-07-09 18:06:25 +0700

    done server api user

---
+++ server/config/dbConn.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+export default connectDB;
--- server/index.js
+++ server/index.js
@@ -1,27 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config()
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+
 import AuthRoute from "./Routes/AuthRoute.js";
-dotenv.config()
+import UserRoute from "./Routes/UserRoute.js";
+import connectDB from "./config/dbConn.js";
+
 // Routes
 const app = express();
+connectDB();
 // middleware
 app.use(bodyParser.json({ limit:'30mb',extended:true }))
 app.use(bodyParser.urlencoded({ extended:true ,limit:'30mb'}))
 const PORT = process.env.PORT || 5000;
-
-
-mongoose
-    .connect(process.env.MONGO_DB,
-    {useNewUrlParser:true,useUnifiedTopology:true,})
-    .then(()=>app.listen(
-        PORT, () => {
-            console.log(`running on port ${PORT}`);
-        }
-    ))
-    .catch((error)=>{
-        console.log(error);
-    })
-
 app.use('/auth',AuthRoute)
+app.use('/user',UserRoute)
+
+mongoose.connection.once('open', () => {
+    console.log('connect to mongodb.');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  });
+
